698a27f5a9d114c6324289a330778f0216bf2709
Yang Liu <ylt5b@mail.missouri.edu>
2015-01-30 15:37:24 -0600

    ylt5b.c created

---
+++ sentences/ylt5b.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int yangSentence1()
+{
+	printf(" Kangaroos likes to sleep\n");
+	return 1;
+}
+
+int yangSentence2()
+{
+	printf("Blue and Black Tigers like to dance\n");
+	return 1;
+}
+
