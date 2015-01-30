ea455132c664ff6078d5b362fd40fceeababb697
wfbc77 <wfbc77@mail.missouri.edu>
2015-01-30 15:12:29 -0600

    added 2 sentences, updated makefile and header

---
+++ sentences/wfbc77.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int wfbc77Sentence1()
+{
+	printf("\"Dangit monster,\" said our hero, \"I ain't givin you no treefiddy.\"\n");
+	return 1;
+}
+
+int wfbc77Sentence2()
+{
+	printf("\nBut then he also remembered that he didn't particularly care. After all, life is short, and who really has time to worry about these kinds of things?");
+	return 1;
+}
